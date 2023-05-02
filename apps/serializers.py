b85292dbf6ad0fe86545a4474f39b593cd208249
vivek <vkjangid056@gmail.com>
2023-05-03 00:20:59 +0530

    serializer comments

--- apps/serializers.py
+++ apps/serializers.py
@@ -2,6 +2,14 @@ from rest_framework import serializers
 from .models import Products
 
 class ProductSerializer(serializers.ModelSerializer):
+    """
+    A serializer for the Products model, to convert data to and from JSON format.
+    """
     class Meta:
         model = Products
         fields = "__all__"
+        """
+        Specifies the model to use and the fields to include in the serialized output.
+        In this case, all the fields of the Products model are included.
+        """
+
