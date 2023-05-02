3afde76be85232fedcf601f42951c17e18f1a376
vivek <vkjangid056@gmail.com>
2023-05-03 00:26:34 +0530

    models.py comments

--- apps/models.py
+++ apps/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 
-# Create your models here.
 class Products(models.Model):
-
+    """
+    A model representing a product for sale.
+    """
     CATEGORY = (('cloths', 'CLOTHS'),
               ('grocery', 'GROCERY'),
               ('others', 'OTHERS')) 
@@ -10,6 +11,10 @@ class Products(models.Model):
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     quantity = models.PositiveIntegerField()
-    category = models.CharField(max_length=10, choices= CATEGORY, default= None)
+    category = models.CharField(max_length=10, choices=CATEGORY, default=None)
+
     def __str__(self):
+        """
+        Returns the name of the product as a string when the object is referenced.
+        """
         return self.name
